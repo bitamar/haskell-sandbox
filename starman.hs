c5ef016b34bf6908fe901e806daec1b78376fbe2
Itamar Shapira Bar-Lev <i@itu.sh>
2017-09-23 22:37:47 +0200

    Adding starman

---
+++ starman.hs
@@ -0,0 +1,59 @@
+module Main where
+
+import Network.HTTP
+
+
+main :: IO ()
+main =
+  do
+    putStrLn "Loading a word. This may take a couple of seconds."
+    secret <- getRandomWord
+    starman secret 8
+
+
+starman :: String -> Int -> IO ()
+starman secret =
+  let
+    display = replicate (length secret) '-'
+  in
+    mkTurn secret display
+
+
+getRandomWord :: IO String
+getRandomWord =
+  let
+    request = getRequest "http://setgetgo.com/randomword/get.php"
+  in
+    simpleHTTP request >>= getResponseBody
+
+
+check :: String -> String -> Char -> (Bool, String)
+check secret display guess =
+  let
+    exist = elem guess secret
+    newDisplay = [ if guess == s then s else d | (s, d) <- zip secret display]
+  in
+    (exist, newDisplay)
+
+
+mkTurn :: String -> String -> Int -> IO ()
+mkTurn secret display turn
+  | turn == 0 =
+    do
+      putStr "You lose. It was: "
+      putStrLn secret
+  | display == secret =
+    do
+      putStr secret
+      putStrLn "! You win."
+  | otherwise =  mkGuess secret display turn
+
+
+mkGuess :: String -> String -> Int -> IO ()
+mkGuess secret display turn =
+  do putStrLn (display ++ "  " ++ replicate turn '*')
+     putStr "  Enter your guess: "
+     q <- getLine
+     let (correct, display') = check secret display (head q)
+     let n' = if correct then turn else turn - 1
+     mkTurn secret display' n'
